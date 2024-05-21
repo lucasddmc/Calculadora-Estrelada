e54c28e4d7b835dd596018aac16eae8c9cb0a7b2
Lucas Carvalho <lucasddmc@outlook.com>
2024-05-21 18:47:34 -0300

    Add xcode project

---
+++ Calculadora Estrelada/Calculadora Estrelada/Calculadora_EstreladaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculadora_EstreladaApp.swift
+//  Calculadora Estrelada
+//
+//  Created by Lucas Dantas de Moura Carvalho on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculadora_EstreladaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculadora Estrelada/Calculadora Estrelada/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculadora Estrelada
+//
+//  Created by Lucas Dantas de Moura Carvalho on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
